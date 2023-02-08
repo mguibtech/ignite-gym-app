246f5cf23238633f8a1c3192d415f7cdf8e9f212
mguibtech <mguibtech@gmail.com>
2023-02-07 23:27:55 -0400

    Using theme in react native base

--- App.tsx
+++ App.tsx
@@ -2,19 +2,23 @@
 import {  Text, View, StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider, Box } from "native-base";
+import { Loading } from '@components/Loading';
+
+import { THEME } from './src/theme';''
 
 export default function App() {
 
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <View/> : <View/>}      
+      {fontsLoaded ? <View/> : <Loading/>}      
+      
     </NativeBaseProvider>
   );
 }
