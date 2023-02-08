19efd6437f8a7023a049c420bc92ab898094969f
mguibtech <mguibtech@gmail.com>
2023-02-07 23:51:55 -0400

    Creating logo of app

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,6 @@
 import { Spinner, Center } from "native-base";
 
+
 export function Loading() {
     return (
         <Center flex={1} bg="gray.700">
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,10 @@
-import { VStack, Image } from 'native-base';
+import { VStack, Image, Text, Center } from 'native-base';
 
-import BackgroundImg from  '@assets/background.png'
+import BackgroundImg from '@assets/background.png'
+import LogoSvg from '@assets/logo.svg'
 
-export function SignIn(){
-    return(
+export function SignIn() {
+    return (
         <VStack flex={1} bg="gray.600">
             <Image
                 source={BackgroundImg}
@@ -11,6 +12,16 @@ export function SignIn(){
                 resizeMode='contain'
                 position="absolute"
             />
+
+            <Center my={24}>
+                <LogoSvg />
+
+                <Text color="gray.100" fontSize="sm">
+                    Treine sua mente e o seu corpo.
+                </Text>
+            </Center>
+
+
 
         </VStack>
     );
